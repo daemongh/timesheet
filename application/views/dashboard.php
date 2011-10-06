acb673908f68cc19eb02d52b195ddbd9bdd853cb
Tom <thomas.r.martin@gmail.com>
2011-10-05 23:36:16 -0400

    prototype of dashboard idea

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -10,5 +10,156 @@
 <h1>Timesheet Dashboard</h1>
 <h3>Welcome, <?php echo $user; ?></h3>
 
+
+<table>
+	<thead>
+		<tr>
+			<td>Main Project</td>
+			<td>Sub Project</td>
+			<td>Notes</td>
+			<td></td>
+			<td>Sunday</td>
+			<td>Monday</td>
+			<td>Tuesday</td>
+			<td>Wednesday</td>
+			<td>Thursday</td>
+			<td>Friday</td>
+			<td>Saturday</td>
+			<td>Total Hours</td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>
+				<select>
+					<option>Project 1</option>
+				</select>
+			</td>
+			<td>
+				<select> 
+					<option>Sub Project 1</option> 
+				</select>
+			</td>
+			<td><textarea cols = 15 rows = 2></textarea></td>
+			<td></td>
+			<td>
+				<select>
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+					<option>6</option>
+					<option>7</option>
+					<option>8</option>
+					<option>9</option>
+					<option>10</option>
+					<option>11</option>
+					<option>12</option>
+				</select>
+			</td>
+			<td>
+				<select>
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+					<option>6</option>
+					<option>7</option>
+					<option>8</option>
+					<option>9</option>
+					<option>10</option>
+					<option>11</option>
+					<option>12</option>
+				</select>
+			</td>
+			<td>
+				<select>
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+					<option>6</option>
+					<option>7</option>
+					<option>8</option>
+					<option>9</option>
+					<option>10</option>
+					<option>11</option>
+					<option>12</option>
+				</select>
+			</td>
+			<td>
+				<select>
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+					<option>6</option>
+					<option>7</option>
+					<option>8</option>
+					<option>9</option>
+					<option>10</option>
+					<option>11</option>
+					<option>12</option>
+				</select>
+			</td>
+			<td>
+				<select>
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+					<option>6</option>
+					<option>7</option>
+					<option>8</option>
+					<option>9</option>
+					<option>10</option>
+					<option>11</option>
+					<option>12</option>
+				</select>
+			</td>
+			<td>
+				<select>
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+					<option>6</option>
+					<option>7</option>
+					<option>8</option>
+					<option>9</option>
+					<option>10</option>
+					<option>11</option>
+					<option>12</option>
+				</select>
+			</td>
+			<td>
+				<select>
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+					<option>6</option>
+					<option>7</option>
+					<option>8</option>
+					<option>9</option>
+					<option>10</option>
+					<option>11</option>
+					<option>12</option>
+				</select>
+			</td>
+			<td>XXX Hours</td>
+		</tr>
+	</tbody>
+</table>
+
+
+
 </body>
 </html>
